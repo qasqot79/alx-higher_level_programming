c04da329a8e647d3463970b66811604199fdfa32
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2022-11-29 14:18:26 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) is not 'q' and chr(letter) is not 'e':
-        print("{}".format(chr(letter)), end="")
+for i in range(ord('a'), ord('z') + 1):
+        if i == ord('q') or i == ord('e'):
+                continue
+        print("{0:c}".format(i), end="")
+# ord receives a char and return asci value integer
