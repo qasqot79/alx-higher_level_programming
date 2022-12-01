172bcbd0cc3a9cca392db7f2194fb1fba00176d8
qasqot79 <cird2007@yahoo.com>
2022-12-01 06:29:39 -0800

    0x02-python-import_modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) is 1:
+        print("{:d} arguments.".format(len(sys.argv) - 1))
+    elif len(sys.argv) is 2:
+        print("{:d} argument:".format(len(sys.argv) - 1))
+        print("{:d}: {}".format(len(sys.argv) - 1, sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {}".format(i, sys.argv[i]))
