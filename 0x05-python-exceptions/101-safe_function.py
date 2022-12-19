5bcb91dd2296455f51ba4ca864b3a00fb1847739
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2022-12-19 13:07:53 +0100

    Update 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+
 import sys
+
 
 def safe_function(fct, *args):
     """Executes a function safely.
     Args:
         fct: The function to execute.
         args: Arguments for fct.
-    Returns:
+  
+  Returns:
         If an error occurs - None.
         Otherwise - the result of the call to fct.
     """
