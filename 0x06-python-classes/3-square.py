0885ef7023c07f5315043ccddf67831ad3f377ee
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2022-12-20 14:34:45 +0100

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # A class Square that defines a square
 
-
 class Square:
     def __init__(self, size=0):  # init allow square class to be used
         self.__size = size   # asign private instance attribute size
@@ -13,4 +12,3 @@ class Square:
     def area(self):
         area = self.__size * self.__size
         return area
-
