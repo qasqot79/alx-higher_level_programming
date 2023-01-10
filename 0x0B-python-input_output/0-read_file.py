3f93e5cfa01b99b5f3e14aaa71656351de894a15
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-01-10 17:18:18 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,7 +3,6 @@
 
 
 def read_file(filename=""):
-    """Prints the contents of a UTF8 text file
-    """
+    """Prints the contents of a UTF8 text file"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
