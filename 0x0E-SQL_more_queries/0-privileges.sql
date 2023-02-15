c696912a2322b2254d6924f8c3f7dc2fcda99a44
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-02-15 19:16:32 +0100

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- Lists all privileges of the users user_0d_1 and user_0d_2.
 -- if the server is in the local host.
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
