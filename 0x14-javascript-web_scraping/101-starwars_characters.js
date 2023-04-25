7c98d7738eef74b6ab32c1c2be6d58ea83b47431
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-04-25 15:06:04 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,47 +1,43 @@
 #!/usr/bin/node
-const base64 = require('base-64');
+const process = require('process');
 const request = require('request');
-const utf8 = require('utf8');
+let order = [];
+let responses = {};
 
-let promise = new Promise(function (resolve, reject) {
-  const token = utf8.decode(base64.encode(`${process.argv[2]}:${process.argv[3]}`));
-  const options = {
-    url: 'https://api.twitter.com/oauth2/token',
-    headers: {
-      Authorization: `Basic ${token}`,
-      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-    },
-    form: {
-      grant_type: 'client_credentials'
-    }
-  };
-  request.post(options, function (error, response, body) {
-    if (!error) {
-      resolve(JSON.parse(body).access_token);
-    }
-  });
-});
-
-promise.then(
-  result => search(result),
-  error => console.log(error)
-);
-
-function search (bearer) {
-  const options = {
-    url: 'https://api.twitter.com/1.1/search/tweets.json',
-    headers: {
-      Authorization: `Bearer ${bearer}`
-    },
-    qs: {
-      q: process.argv[4],
-      count: '5'
-    }
-  };
-  request.get(options, function (error, response, body) {
-    if (!error) {
-      const tweets = JSON.parse(body).statuses;
-      tweets.forEach((t) => console.log(`[${t.id}] ${t.text} by ${t.user.name}`));
+function getCharName (charUrl) {
+  let val;
+  val = request(charUrl, function (error, response, body) {
+    if (error != null) {
+      console.log(error);
+    } else {
+      let data = JSON.parse(body);
+      val = data['name'];
+      responses[charUrl] = val;
     }
   });
 }
+
+function doParse () {
+  let movie = process.argv[2];
+  let url = 'https://swapi.co/api/films/' + movie;
+
+  request(url, function (error, response, body) {
+    if (error != null) {
+      console.log(error);
+    } else {
+      let data = JSON.parse(body);
+      data['characters'].forEach(function (charUrl) {
+        order.push(charUrl);
+        getCharName(charUrl);
+      });
+    }
+  });
+}
+
+doParse();
+setTimeout(function () {
+  // some stuff
+  order.forEach(function (url) {
+    console.log(responses[url]);
+  });
+}, 5000);
